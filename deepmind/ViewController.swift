5a132e262036acd66d114c5c73e01359f8c63f42
mac88 <mac88@host168.advance.com.ar>
2017-07-04 08:18:15 +0900

    Initial Commit

---
+++ deepmind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  deepmind
+//
+//  Created by mac88 on 2017. 7. 4..
+//  Copyright © 2017년 mac88. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
